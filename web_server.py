7c1d0d87aa3da5b2deaa5579d76dbf577624e30a
cobit-git <connercobit@gmail.com>
2021-01-19 18:41:31 -0800

    Feb 20 3

--- web_server.py
+++ web_server.py
@@ -7,15 +7,15 @@ import threading
 import asyncio
 import time 
 
+from adafruit_servokit import ServoKit
+from cobit_car_motor_l9110 import CobitCarMotorL9110
+
 class DriveAPI(RequestHandler):
     def get(self):
-        #data = {}
         self.render("templates/vehicle_main.html")
 
 class WebSocketDriveAPI(tornado.websocket.WebSocketHandler):
-    #wsclients = []
     def open(self):
-        #self.wsclients.append(self)
         print("New client connected")
 
     def on_message(self, message):
@@ -30,6 +30,7 @@ class cobit_car_server(tornado.web.Application):
     def __init__(self, port = 8887):
         
         self.angle = 0.0
+        self.throttle = 0.0
         this_dir = os.path.dirname(os.path.realpath(__file__))
         self.static_file_path = os.path.join(this_dir, 'templates', 'static')
 
@@ -53,6 +54,24 @@ class cobit_car_server(tornado.web.Application):
     def get_angle(self):
         return self.angle
 
+    def get_throttle(self):
+        return self.throttle
+
+class vehicle_control:
+    def __init__(self):
+        self.motor = CobitCarMotorL9110()
+        self.servo = ServoKit(channels=16)
+
+    def motor_control(self, angle, throttle):
+        if self.throttle > 0:
+            self.motor.motor_all_start(int(self.throttle*100))
+        else:
+            self.motor.motor_all_start(0)
+        angle_x = self.angle*100 + 90
+        if angle_x > 30 and angle_x < 150:
+            self.servo.servo[0].angle = angle_x
+
+
 
 if __name__=="__main__":
     app = cobit_car_server()
@@ -60,37 +79,8 @@ if __name__=="__main__":
     t.daemon = True
     t.start()
 
-    while True:
-        time.sleep(1)
-        print(app.get_angle())
-'''
-class cobit_car_server(threading.Thread):
-    def __init__(self, port = 8887):
-        
-        threading.Thread.__init__(self) 
-        this_dir = os.path.dirname(os.path.realpath(__file__))
-        self.static_file_path = os.path.join(this_dir, 'templates', 'static')
-
-        self.app = Application([
-            (r"/", RedirectHandler, dict(url="/drive")),
-            (r"/drive", DriveAPI),
-            (r"/static/(.*)", StaticFileHandler,
-                {"path": self.static_file_path}),
-            (r"/wsDrive", WebSocketDriveAPI),
-        ])
-
-
-    def run(self):
-        asyncio.set_event_loop(asyncio.new_event_loop())
-        self.app.listen(8887)
-        tornado.ioloop.IOLoop.current().start()
-
-
-if __name__=="__main__":
-    t = cobit_car_server()
-    t.daemon = True
-    t.start()
+    vc = vehicle_control()
 
     while True:
-        time.sleep(1)
-'''
+        time.sleep(0.1)
+        vc.motor_control(app.get_angle(),app.get_throttle())
